d696c637b7dd2ceda57cac638d7fe01733c9078d
villavong <asyao@naver.com>
2016-12-12 03:59:03 +0900

    production true email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'http://www.tweedlemate.com/' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
